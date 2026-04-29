016ac2c7076505cc6dca41897f851b63ac585af3
none34211 <shruti.b.0227@iibm.in>
2026-04-29 11:42:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function startNow() {
-  alert('Welcome to Veileder 🚀 Let us grow your brand');
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function sendEmail() {
+  window.location.href = 'mailto:veileder30@gmail.com';
 }
