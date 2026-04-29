9e79e57244d9dcd66247713b0b0a6507d826d5a7
none34211 <shruti.b.0227@iibm.in>
2026-04-29 12:03:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function email() {
   window.location.href = 'mailto:veileder30@gmail.com';
 }
 
-/* SCROLL ANIMATION */
 window.addEventListener('scroll', () => {
   document.querySelectorAll('.reveal').forEach(el => {
     const top = el.getBoundingClientRect().top;
